ff9a6159903da1e9acef15625561d6a86152c1c9
Joel Salzmann <salzmann.joel@gmail.com>
2018-03-07 14:47:59 +0100

    added xml and xslt for each building and first version of building click

--- map.js
+++ map.js
@@ -1,10 +1,50 @@
 function init() {
     var buildings = document.getElementsByClassName("building");
-    for (var i=0; i < buildings.length; i++) {
-        buildings[i].addEventListener("click", buildingClicked);
+    for (var i = 0; i < buildings.length; i++) {
+        buildings[i].addEventListener("click", function(){
+            console.log(this.id + " got clicked");
+            getContent(this.id);
+        });
     }
 }
 
-function buildingClicked() {
-    alert("Hello World");
+function loadXMLDoc(file)
+{
+    if (window.ActiveXObject)
+    {
+        xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } else
+    {
+        xhttp = new XMLHttpRequest();
+    }
+    xhttp.open("GET", file, false);
+    try {
+        xhttp.responseType = "msxml-document";
+    } catch (err) {
+        console.log("Coult not load xml-file");
+    } // Helping IE11
+    xhttp.send("");
+    return xhttp.responseXML;
+}
+
+function getContent(filename)
+{
+    xml = loadXMLDoc("map/"+filename+".xml");
+    xsl = loadXMLDoc("map/"+filename+".xsl");
+// code for IE
+    if (window.ActiveXObject || xhttp.responseType == "msxml-document")
+    {
+        ex = xml.transformNode(xsl);
+        //document.getElementById("example").innerHTML = ex;
+        alert(ex);
+    }
+// code for Chrome, Firefox, Opera, etc.
+    else if (document.implementation && document.implementation.createDocument)
+    {
+        xsltProcessor = new XSLTProcessor();
+        xsltProcessor.importStylesheet(xsl);
+        resultDocument = xsltProcessor.transformToFragment(xml, document);
+        //document.getElementById("example").appendChild(resultDocument);
+        alert(resultDocument);
+    }
 }
---
+++ map/kletterhalle.xsl
@@ -0,0 +1,16 @@
+<?xml version="1.0"?>
+
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
+				xmlns:sc="https://thecell.eu/xmlsportcenter" 
+				xmlns:xhtml="http://www.w3.org/1999/xhtml"
+				xmlns:xs="http://www.w3.org/2001/XMLSchema"
+				version="1.0">
+
+	<xsl:output method="html"/>
+    <xsl:template match="sc:index">
+        <div>
+            Hello World
+        </div>
+    </xsl:template>
+    
+</xsl:stylesheet>
